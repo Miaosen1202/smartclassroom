ec00ec0dfb95bd76ffa379c185c31af5e96b609d
miao <miaosen1202@163.com>
2018-09-13 16:31:30 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	config/dev.env.js
    #	config/prod.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,12 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  /*NODE_ENV: '"development"'*/
-<<<<<<< HEAD
   /*NODE_ENV: '"http://192.168.8.93:8080"'*/
   NODE_ENV: '"http://localhost:8088"'
-=======
-  // NODE_ENV: '"http://192.168.8.93:8080"'
-  NODE_ENV: '"http://localhost:8080"'
->>>>>>> origin/master
+
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,5 @@
 'use strict'
 module.exports = {
-  /*NODE_ENV: '"production"'*/
-<<<<<<< HEAD
  /* NODE_ENV: '"http://192.168.8.93:8080"'*/
   NODE_ENV: '"http://localhost:8088"'
-=======
-  // NODE_ENV: '"http://192.168.8.93:8080"'
-  NODE_ENV: '"http://localhost:8080"'
->>>>>>> origin/master
 }

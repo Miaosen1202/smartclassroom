0f435524cc5931e0ab4dcc17699e3a99d1b77e4b
zz <zhangzhen2540@qq.com>
2018-09-14 19:49:06 +0800

    update

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   /*NODE_ENV: '"production"'*/
-  /*NODE_ENV: '"http://192.168.8.93:8080"'*/
-  NODE_ENV: '"http://localhost:8088"'
+  NODE_ENV: '"http://192.168.8.93:8080"'
+  // NODE_ENV: '"http://localhost:8088"'
 }
